1f835f778312de2516e11aef353541866829fd6c
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-01-10 19:35:46 +0530

    Add Advanced C Assignment Programs

---
+++ Assignments_10/Program10_03.c
@@ -0,0 +1,75 @@
+/* 
+    3. Write a program which accept range from user and return addition of all numbers in between that range. (Range should contains positive numbers only).
+
+    Input  : 23 30
+    Output : 212
+
+    Input  : 10 18
+    Output : 126
+
+    Input  : -10 2
+    Output : Invalid range
+
+    Input  : 90 18
+    Output : Invalid range
+
+*/
+
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name   :   RangeSum
+//  Description     :   Returns the sum of all positive numbers in the given range.
+//  Input           :   Integer, Integer
+//  Output          :   Integer (Sum of numbers in the specified range).
+//  Author          :   Gavade Somanath Vilas
+//  Date            :   10/01/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//  Function Definition
+int RangeSum(int iStart, int iEnd)
+{
+    int iCnt = 0, iSum = 0;
+
+    // Invalid range condition
+    if(iStart < 0 || iEnd < 0 || iStart > iEnd)
+    {
+        printf("Invalid Range\n");
+        return 0;
+    }
+    
+    // Traverse the range and calculate sum.
+    for(iCnt = iStart; iCnt <= iEnd; iCnt++)
+    {   
+            iSum = iSum + iCnt;
+    }
+
+    return iSum;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Application Name    :   Addition of Numbers in Range.
+//  Description         :   This application accepts a starting and ending number and displays the addition of all numbers within that range.
+//                          (Range must contain positive numbers only)
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0,iRet = 0;
+
+    printf("Enter starting Point : \n");
+    scanf("%d", &iValue1);
+
+    printf("Enter Ending point : \n");
+    scanf("%d", &iValue2);
+
+    iRet = RangeSum(iValue1, iValue2);     //  Function Call
+
+    printf("Addition is : %d\n", iRet);
+
+    return 0;
+}
